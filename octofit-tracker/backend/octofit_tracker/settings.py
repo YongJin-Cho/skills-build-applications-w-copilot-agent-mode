ccc3754e4741bb497618f3b014e2d868bc4bc547
Yong Jin, Cho <drajin.cho@bosornd.com>
2025-04-16 22:40:51 +0000

    Push changes to views.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-t0u-#jtppqu9fb9-%(pepq^5k1_4du=dgx+i8ec@0+$l&g3y8k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[REPLACE-THIS-WITH-YOUR-CODESPACE-NAME]-8000.app.github.dev']
 
 
 # Application definition
